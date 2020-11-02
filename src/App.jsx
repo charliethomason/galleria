e6cea34b1a66cb62b1d6b44b2fbe4ca4e036f26d
Charlie Thomason <thomason.charlie@gmail.com>
2020-11-02 09:48:01 -0600

    basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,21 @@ export default class App extends Component {
     return (
       <main>
         <h1>Photo Gallery</h1>
-        {images.keys().map((img, i) => {
-          return <img src={`./img/photos/${img}`} alt={img} key={img} style={{ height: "200px" }} />;
-        })}
+        <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", maxWidth: "1000px", margin: "0 auto" }}>
+          {images.keys().map((img, i) => {
+            return (
+              <div key={img} style={{ flex: "0 1 auto", height: "200px" }}>
+                <a href="#" style={{ display: "block", height: "100%" }}>
+                  <img
+                    src={`./img/photos/${img}`}
+                    alt={img}
+                    style={{ dipslay: "block", height: "100%", maxWidth: "100%" }}
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
       </main>
     );
   }
