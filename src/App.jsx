f5d89316158b09c38c3d4f1413f7800d38c6a308
Charlie Thomason <thomason.charlie@gmail.com>
2020-11-17 17:45:36 -0600

    built out basic galleria row math logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
+import photos from "./photos";
 
 export default class App extends Component {
-  render() {
-    const images = require.context('./img/photos', false);
-    return (
-      <main>
-        <h1>Photo Gallery</h1>
-        <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", maxWidth: "1000px", margin: "0 auto" }}>
-          {images.keys().map((img, i) => {
+  renderGalleria() {
+    function getTotalWidth(row) {
+      return row.reduce((sum, r) => {
+        return sum + r.width;
+      }, 0);
+    }
+    const photosInRows = photos.reduce((rows, img) => { 
+      if (!rows.length) {
+        rows.push([img]);
+        return rows;
+      } else {
+        for (const [i, row] of rows.entries()) {
+          const currWidths = getTotalWidth(row);
+          const isLastRow = i === rows.length-1;
+          if (currWidths >= 2400) {
+            if (isLastRow) {
+              rows.push([img]);
+              break;
+            } else {
+              continue;
+            }
+          } else {
+            row.push(img);
+            break;
+          }
+        }
+        return rows;
+      }
+    }, []);
+    const imgHeight = 600;
+    const rowWidth = 1000;
+    const galleria = photosInRows.map((row, r) => {
+      const totalWidth = getTotalWidth(row);
+      const rowHeight = rowWidth / (totalWidth / imgHeight);
+      return (
+        <div key={`row-${r}`} className="galleria-row">
+          {row.map((img, i) => {
+            const imgStyle = {
+              display: "inline-block",
+              height: rowHeight,
+              width: img.width / (imgHeight / rowHeight)
+            };
             return (
-              <div key={img} style={{ flex: "0 1 auto", height: "200px" }}>
-                <a href="#" style={{ display: "block", height: "100%" }}>
-                  <img
-                    src={`./img/photos/${img}`}
-                    alt={img}
-                    style={{ dipslay: "block", height: "100%", maxWidth: "100%" }}
-                  />
-                </a>
-              </div>
+              <a href="#" key={`img-${i}`} className="galleria-img" style={imgStyle}>
+                <img src={`./img/photos/${img.file}.jpg`} />
+              </a>
             );
           })}
+        </div>
+      );
+    });
+    return galleria;
+  }
+
+  render() {
+    return (
+      <main className="wrapper">
+        <h1>Photo Gallery</h1>
+        <div className="galleria">
+          {this.renderGalleria()}
         </div>
       </main>
     );
---
+++ src/photos.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    file: "austria",
+    width: 899
+  },
+  {
+    file: "blackwaterfalls",
+    width: 800
+  },
+  {
+    file: "bloomingdale",
+    width: 801
+  },
+  {
+    file: "camdenlighthouse",
+    width: 906
+  },
+  {
+    file: "ei-sunrise",
+    width: 800
+  },
+  {
+    file: "ei-sunset",
+    width: 801
+  },
+  {
+    file: "englishportfolio",
+    width: 853
+  },
+  {
+    file: "frozenpromontory",
+    width: 800
+  },
+  {
+    file: "lugano",
+    width: 800
+  },
+  {
+    file: "nottevenezia",
+    width: 800
+  },
+  {
+    file: "openrange",
+    width: 821
+  },
+  {
+    file: "rotr-pano",
+    width: 1331
+  },
+  {
+    file: "sandiego",
+    width: 899
+  },
+  {
+    file: "venezia",
+    width: 801
+  },
+  {
+    file: "wahkeena",
+    width: 450
+  }
+];
